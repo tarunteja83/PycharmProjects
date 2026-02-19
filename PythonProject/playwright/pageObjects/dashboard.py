226d3e74fe34bca820a5a39023a5cb492f105115
tarunteja83 <tarunteja8383@gmail.com>
2026-02-19 14:51:30 +0530

    Update dashboard.py

--- PythonProject/playwright/pageObjects/dashboard.py
+++ PythonProject/playwright/pageObjects/dashboard.py
@@ -7,5 +7,6 @@ class DashboardPage:
 
     def clickOrders(self):
         self.page.get_by_role("button", name="ORDERS").click()
+
         Order_History_Page = OrderHistoryPage(self.page)
         return Order_History_Page
